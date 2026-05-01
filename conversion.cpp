d462a1443660233fea5f98bd75f35a792dc4cdd1
Tomás Maller Vulcano <tmaller@frba.utn.edu.ar>
2026-05-01 19:30:21 -0300

    Add files via upload

---
+++ conversion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float mm;
+    cout << "Ingrese valor en mm: ";
+    cin >> mm;
+    cout << mm << " mm equivalen a " << mm / 25.4 << " pulgadas." << endl;
+    return 0;
+}
